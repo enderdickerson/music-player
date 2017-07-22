7c25679939996ece74357d2794753e6ba511142c
Andrew Dickerson <olah89@gmail.com>
2017-07-22 10:05:44 -0700

    Rebuild with angular 4 cli

---
+++ src/app/music/music-player/music-player.component.ts
@@ -0,0 +1,17 @@
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+
+@Component({
+  selector: 'music-player',
+  templateUrl: './music-player.component.html',
+  styleUrls: ['./music-player.component.class']
+})
+
+export class MusicPlayerComponent {
+  @Input() paused;
+
+  @Output() backward = new EventEmitter();
+  @Output() pauseplay = new EventEmitter();
+  @Output() forward = new EventEmitter();
+  @Output() random = new EventEmitter();
+  @Output() stop = new EventEmitter();
+}
---
+++ src/app/music/music.module.ts
@@ -0,0 +1,42 @@
+// Third party imports
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from "@angular/http";
+import { CommonModule } from '@angular/common';
+// PrimeNG autocomplete for search
+import { AutoCompleteModule } from 'primeng/primeng';
+
+// Custom imports
+import { MusicSearchComponent } from './music-search/music-search.component';
+import { MusicPlayerComponent } from './music-player/music-player.component';
+import { MusicDetailsComponent } from './music-details/music-details.component';
+import { MusicProgressComponent } from './music-progress/music-progress.component';
+// import { MusicService } from './shared/music.service';
+// import { ApiService } from './shared/api.service';
+
+@NgModule({
+    imports: [
+      FormsModule,
+      AutoCompleteModule,
+      HttpModule,
+      CommonModule
+    ],
+    exports: [
+      MusicSearchComponent,
+      MusicDetailsComponent,
+      MusicPlayerComponent,
+      MusicProgressComponent
+    ],
+    declarations: [
+      MusicSearchComponent,
+      MusicDetailsComponent,
+      MusicPlayerComponent,
+      MusicProgressComponent
+    ],
+    providers: [
+      // ApiService,
+      // MusicService
+    ],
+})
+
+export class MusicModule { }
