b4585ff8a5245a5910ddec5a2b3b3f1ad7a4789a
Andrew Dickerson <olah89@gmail.com>
2017-07-21 18:14:06 -0700

    Getting electron set up

---
+++ main.js
@@ -0,0 +1,40 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+require('dotenv').config();
+
+require('electron-reload')(__dirname);
+
+let win = null;
+
+app.on('ready', () => {
+  win = new BrowserWindow({width: 1000, height: 600});
+
+  if (process.env.PACKAGE === 'true') {
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'dist/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+  } else {
+      win.loadURL(process.env.HOST);
+      win.webContents.openDevTools();
+  }
+
+  win.on('closed', function() {
+    win = null;
+  });
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+})
